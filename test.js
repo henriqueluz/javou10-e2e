01b088209f3eb15961d653c3ef8742d2426a6468
Henrique Luz <riqueluz@gmail.com>
2017-06-22 16:23:22 -0300

    Exemplo inicial usando firefox

---
+++ test.js
@@ -0,0 +1,17 @@
+var browser = require('webdriverio');
+var options = {
+  desiredCapabilities: {
+    browserName: 'firefox'
+  },
+  reporters: ['spec']
+};
+
+var client = browser.remote(options)
+                    .init()
+                    .url('http://www.google.com')
+                    .pause(3000);
+
+client.getTitle()
+      .then(function(title) {
+          console.log('Title was: ' + title);
+      }).end();
