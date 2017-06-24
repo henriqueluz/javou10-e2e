b82c0bd84c10c78fd539ee4d34bdc755c96d9655
Henrique Luz <riqueluz@gmail.com>
2017-06-24 11:06:25 -0300

    Testes para busca por sobrenome

--- spec/agenda.spec.js
+++ spec/agenda.spec.js
@@ -1,13 +1,26 @@
 describe('Agenda de Contatos', function() {
 
     it('Deveria buscar contato por nome', function() {
+        var filtro = 'Antonio';
         browser.url('/').pause(2000);
-        browser.setValue('#filtroPorNome', 'Antonio').pause(2000);
+        browser.setValue('#filtroPorNome', filtro).pause(2000);
         var rows = $$("table tbody tr");
         var columns = rows[0].$$('td');
         var nomeDoContato = columns[0];
 
-        expect(nomeDoContato.getText()).toBe('Antonio');
+        expect(nomeDoContato.getText()).toBe(filtro);
+    });
+
+    it('Deveria buscar contato por sobrenome', function() {
+        var filtro = 'Cox';
+        browser.url('/').pause(2000);
+        browser.click('//*[@id="ordernacao"]/option[2]').pause(1000);
+        browser.setValue('#filtroPorNome', filtro).pause(2000);
+        var rows = $$("table tbody tr");
+        var columns = rows[0].$$('td');
+        var nomeDoContato = columns[1];
+
+        expect(nomeDoContato.getText()).toBe(filtro);
     });
 
 });
