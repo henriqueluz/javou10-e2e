1d7d205d9fb38c5ceeab6bcade6e8a89b2993990
Henrique Luz <riqueluz@gmail.com>
2017-06-24 10:43:10 -0300

    Buscando contato por nome

--- spec/agenda.spec.js
+++ spec/agenda.spec.js
@@ -3,9 +3,11 @@ describe('Agenda de Contatos', function() {
     it('Deveria buscar contato por nome', function() {
         browser.url('/').pause(2000);
         browser.setValue('#filtroPorNome', 'Antonio').pause(2000);
-        var contatos = browser.getValue('table tbody');
+        var rows = $$("table tbody tr");
+        var columns = rows[0].$$('td');
+        var nomeDoContato = columns[0];
 
-        expect(1).toBe(1);
+        expect(nomeDoContato.getText()).toBe('Antonio');
     });
 
 });
